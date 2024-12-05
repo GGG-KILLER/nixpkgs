95a0f604a7b7fa421937394db0dba561bfff7812
GGG <gggkiller2@gmail.com>
2024-12-05 20:29:28 -0300

    dotnetCorePackages.mkNugetDeps: support loading JSON lockfiles

--- pkgs/build-support/dotnet/make-nuget-deps/default.nix
+++ pkgs/build-support/dotnet/make-nuget-deps/default.nix
@@ -1,23 +1,43 @@
-{ symlinkJoin
-, fetchurl
-, stdenvNoCC
-, lib
-, unzip
-, patchNupkgs
-, nugetPackageHook
-, fetchNupkg
+{
+  symlinkJoin,
+  lib,
+  fetchNupkg,
 }:
-lib.makeOverridable(
-  { name
-  , nugetDeps ? import sourceFile
-  , sourceFile ? null
-  , installable ? false
+lib.makeOverridable (
+  {
+    name,
+    nugetDeps ? null,
+    sourceFile ? null,
+    installable ? false,
   }:
+  let
+    callNupkg = pkg: if lib.isDerivation pkg then pkg else fetchNupkg pkg;
+
+    loadDeps =
+      x:
+      if x == null then
+        [ ]
+      else if lib.isDerivation x then
+        [ x ]
+      else if builtins.isList x then
+        builtins.map callNupkg x
+      else
+        loadDeps (
+          if builtins.isFunction x then
+            x { fetchNuGet = args: fetchNupkg (args // { inherit installable; }); }
+          else if lib.hasSuffix ".json" x then
+            lib.importJSON x
+          else
+            import x
+        );
+
+    deps = if nugetDeps != null then loadDeps nugetDeps else loadDeps sourceFile;
+  in
   (symlinkJoin {
     name = "${name}-nuget-deps";
-    paths = nugetDeps {
-      fetchNuGet = args: fetchNupkg (args // { inherit installable; });
-    };
-  }) // {
-    inherit sourceFile;
+    paths = deps;
   })
+  // {
+    inherit sourceFile;
+  }
+)
